ccbce38f6e8ba724e2f2151b7f0f68173d79187f
Shreya Sharma <shreya1sharma@users.noreply.github.com>
2019-10-10 11:45:08 +0900

    Create tensorboard_in_keras.py

---
+++ tensorboard_in_keras.py
@@ -0,0 +1,29 @@
+'''
+Create a new TensorBoard instance and point it to a log directory where data should be collected.
+Next you need to modify the fit call so that it includes the tensorboard callback.
+Ref:
+https://fizzylogic.nl/2017/05/08/monitor-progress-of-your-keras-based-neural-network-using-tensorboard/
+'''
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from keras.callbacks import TensorBoard
+
+model = Sequential()
+
+model.add(Dense(10, input_shape=(784,)))
+model.add(Activation('softmax'))
+
+model.compile(optimizer='sgd', loss='categorical_crossentropy')
+
+tensorboard = TensorBoard(log_dir="logs/{}".format(time()))
+
+model.fit(x_train, y_train, verbose=1, callbacks=[tensorboard])
+
+
+'''
+Run the following command on terminal
+
+tensorboard --logdir=logs/
+'''
+
